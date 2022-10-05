842289d5200f30a866ded8c44561cc36c284e1a8
oleg4 <oleg4@LAPTOP-MSQS2C0F>
2022-10-05 20:46:33 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp7/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int g=0;
+    
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2("g");
+            if (form2.ShowDialog() == DialogResult.OK)
+            {
+                listBox1.Items.Add(form2.ttext);
+                
+            }
+           
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2("g");
+            if (form2.ShowDialog() == DialogResult.OK)
+            {
+               int p= listBox1.SelectedIndex;
+                listBox1.Items[p] = form2.ttext;
+
+            }
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int p = listBox1.SelectedIndex;
+            listBox1.Items.RemoveAt(p);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+    }
+}
---
+++ WindowsFormsApp7/Form2.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp7
+{
+    public partial class Form2 : Form
+    {
+        public Form2(string labe)
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.DialogResult=DialogResult.OK;
+            
+        }
+        public string ttext
+        {
+            get { return textBox1.Text +", " +textBox2.Text + ", "+ textBox3.Text+"\n"; }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.OK;
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
